547d7134013e8ee3875fce8bcac61884ebe8afce
sandyso <sandyrasom@gmail.com>
2016-03-31 21:47:46 -0400

    updated js/css

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
-	$('#add').click(function() {
-	$('.list').prepend('<li>' + $('#item').val() + '</li>');
-});
-	$('.list').on('click', 'li', function(e) {
-		$(this).css('text-decoration','line-through');
-	});
+    $('#add').click(function() {
+        $('.list').prepend('<li>' + $('#item').val() + '</li>');
+    })
+    
+    .mousedown(function() {
+    	$('.list').on('click', 'li', function(e) {
+    		$(this).css('background', 'black');
+    	})
+    })
 });
